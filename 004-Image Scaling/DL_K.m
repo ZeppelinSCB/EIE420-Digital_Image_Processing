c3a2caee3c21d08491a396d190bd80ac74b4bd4f
Karl Tang <littlejun3004@outlook.com>
2025-04-26 14:35:57 +0800

    fix: fix the issue with DL, it turns out causing by error assignment of r,c and x,y

--- 004-Image Scaling/DL_K.m
+++ 004-Image Scaling/DL_K.m
@@ -8,15 +8,17 @@ function [imScaled] = DL_K(imIn, imOut, scale)
     imEnlarged = [
         imEnlarged(:,1,:), imEnlarged, imEnlarged(:,end,:);
     ];
-    for row = 1:size(imOut, 1)-1
+    for row = 1:size(imOut, 1)
         for col = 1:size(imOut, 2)
             rOrg = 1+row/scale;
             cOrg = 1+col/scale;
+            x = (1-mod(cOrg, 1));
+            y = (1-mod(rOrg, 1));
             %NE
-            pixNE = imEnlarged(floor(rOrg),floor(cOrg),:).*mod(rOrg, 1).*mod(cOrg, 1);
-            pixNW = imEnlarged(floor(rOrg),floor(cOrg)+1,:).*mod(rOrg, 1).*(1-mod(cOrg, 1));
-            pixSE = imEnlarged(floor(rOrg)+1,floor(cOrg),:).*(1-mod(rOrg, 1)).*mod(cOrg, 1);
-            pixSW = imEnlarged(floor(rOrg)+1,floor(cOrg)+1,:).*(1-mod(rOrg, 1)).*(1-mod(cOrg, 1));
+            pixNE = imEnlarged(floor(rOrg),floor(cOrg),:).*x.*y;
+            pixNW = imEnlarged(floor(rOrg),floor(cOrg)+1,:).*(1-x).*(y);
+            pixSE = imEnlarged(floor(rOrg)+1,floor(cOrg),:).*(x).*(1-y);
+            pixSW = imEnlarged(floor(rOrg)+1,floor(cOrg)+1,:).*(1-x).*(1-y);
             imScaled(row, col,:) = pixNE + pixNW + pixSE + pixSW;
         end
     end
